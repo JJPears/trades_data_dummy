60c83e21a7b6740802b3d083e5f58091d0b8bd0f
Josh <josh.pears1997@gmail.com>
2026-04-30 14:43:23 +0100

    Creating centralised trades collection objects to handle functional logic

---
+++ models/trade_collection.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from typing import Any
+
+
+class TradeCollection(list):
+    def to_df(self) -> pd.DataFrame:
+        rows = [t.model_dump() for t in self]
+        return pd.DataFrame(rows)
+
+    def to_dicts(self) -> list[dict[str, Any]]:
+        return [t.model_dump() for t in self]
+
+    def to_csv(self, file_path):
+        df = self.to_df()
+        df.to_csv(file_path, index=False)
--- trade_service.py
+++ trade_service.py
@@ -1,5 +1,5 @@
 from common.enums import ProductType
-from models.asset_classes import Trade
+from models.trade_collection import TradeCollection
 from factories.trade_factory import (
     create_bullet_bond_trade,
     create_ir_swap_trade,
@@ -8,8 +8,6 @@ from factories.trade_factory import (
 )
 
 import random
-import pandas as pd
-from typing import Any
 
 FACTORY_MAP = {
     ProductType.BOND_FIXED: create_bullet_bond_trade,
@@ -26,9 +24,9 @@ def create_trades(
     n: int = 15,
     product_types: list[ProductType] | None = None,
     sort: bool = True,
-) -> list[Trade]:
+) -> TradeCollection:
 
-    trades = []
+    trades = TradeCollection()
     available_types = product_types or ALL_PRODUCT_TYPES
     # Potentially need error handling
 
@@ -37,22 +35,7 @@ def create_trades(
         trade = FACTORY_MAP[product_type]()
         trades.append(trade)
 
-    return (
-        sorted(trades, key=lambda t: t.trade_date, reverse=True)
-        if sort
-        else trades
-    )
+    if sort:
+        trades.sort(key=lambda t: t.trade_date, reverse=True)
 
-
-def to_df(trades: list[Trade]) -> pd.DataFrame:
-    rows = [t.model_dump() for t in trades]
-    return pd.DataFrame(rows)
-
-
-def to_csv(file_path: str, trades: list[Trade]) -> None:
-    df = to_df(trades)
-    df.to_csv(file_path, index=False)
-
-
-def to_dicts(trades: list[Trade]) -> list[dict[str, Any]]:
-    return [t.model_dump() for t in trades]
+    return trades
