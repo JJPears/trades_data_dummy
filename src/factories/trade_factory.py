4099ef16b1ac9b4f6b251a4f1a37e8f3e668f3ef
Josh <josh.pears1997@gmail.com>
2026-05-11 20:09:52 +0100

    Adding trade factory class

--- src/factories/trade_factory.py
+++ src/factories/trade_factory.py
@@ -3,6 +3,7 @@ from src.models.trade_models import (
     IRSwapTrade,
     FXOptionTrade,
     FXTrade,
+    Trade,
 )
 from src.common.utils import (
     generate_trade_id,
@@ -23,92 +24,103 @@ from src.common.utils import (
 from src.common.enums import AssetClass, ProductType, Ccy
 
 
-def create_bullet_bond_trade() -> BulletBondTrade:
-    # This is not a realistic life cycle for our bond and there will be bias due to
-    # maturity date being dependent on trade date
+class TradeFactory:
+    def create(self, product_type: ProductType) -> Trade:
+        match product_type:
+            case ProductType.BOND_FIXED:
+                return self._create_bullet_bond_trade()
+            case ProductType.IR_SWAP:
+                return self._create_ir_swap_trade()
+            case ProductType.FX_OPTION:
+                return self._create_fx_option_trade()
+            case ProductType.FX_SPOT:
+                return self._create_fx_spot_trade()
+            case _:
+                raise ValueError(f"Unsupported product type: {product_type}")
 
-    issue_date = generate_date()
-    trade_date = generate_date(start_date=issue_date)
-    maturity_date = generate_date(start_date=trade_date)
+    def _create_bullet_bond_trade(self) -> BulletBondTrade:
+        # This is not a realistic life cycle for our bond and there will be bias due to
+        # maturity date being dependent on trade date
 
-    bond_data = {
-        "trade_id": generate_trade_id(),
-        "trade_date": trade_date,
-        "asset_class": AssetClass.RATES,
-        "product_type": ProductType.BOND_FIXED,
-        "notional": generate_notional(),
-        "currency": generate_currency(),
-        "issuer": generate_issuer(),
-        "coupon_rate": generate_rate(),
-        "coupon_freq": generate_coupon_frequency(),
-        "issue_date": issue_date,
-        "maturity_date": maturity_date,
-        "day_count": generate_day_count(),
-    }
+        issue_date = generate_date()
+        trade_date = generate_date(start_date=issue_date)
+        maturity_date = generate_date(start_date=trade_date)
 
-    return BulletBondTrade(**bond_data)
+        bond_data = {
+            "trade_id": generate_trade_id(),
+            "trade_date": trade_date,
+            "asset_class": AssetClass.RATES,
+            "product_type": ProductType.BOND_FIXED,
+            "notional": generate_notional(),
+            "currency": generate_currency(),
+            "issuer": generate_issuer(),
+            "coupon_rate": generate_rate(),
+            "coupon_freq": generate_coupon_frequency(),
+            "issue_date": issue_date,
+            "maturity_date": maturity_date,
+            "day_count": generate_day_count(),
+        }
 
+        return BulletBondTrade(**bond_data)
 
-def create_ir_swap_trade() -> IRSwapTrade:
+    def _create_ir_swap_trade(self) -> IRSwapTrade:
 
-    start_date = generate_date()
-    maturity_date = generate_date(start_date=start_date)
+        start_date = generate_date()
+        maturity_date = generate_date(start_date=start_date)
 
-    swap_data = {
-        "trade_id": generate_trade_id(),
-        "trade_date": generate_date(start_date=start_date),
-        "asset_class": AssetClass.RATES,
-        "product_type": ProductType.IR_SWAP,
-        "notional": generate_notional(),
-        "currency": generate_currency(),
-        "fixed_rate": generate_rate(),
-        "float_index": generate_float_index(),
-        "start_date": start_date,
-        "maturity_date": maturity_date,
-        "fixed_leg_freq": generate_coupon_frequency(),
-        "floating_leg_freq": generate_coupon_frequency(),
-        "day_count_fixed": generate_day_count(),
-        "day_count_floating": generate_day_count(),
-    }
+        swap_data = {
+            "trade_id": generate_trade_id(),
+            "trade_date": generate_date(start_date=start_date),
+            "asset_class": AssetClass.RATES,
+            "product_type": ProductType.IR_SWAP,
+            "notional": generate_notional(),
+            "currency": generate_currency(),
+            "fixed_rate": generate_rate(),
+            "float_index": generate_float_index(),
+            "start_date": start_date,
+            "maturity_date": maturity_date,
+            "fixed_leg_freq": generate_coupon_frequency(),
+            "floating_leg_freq": generate_coupon_frequency(),
+            "day_count_fixed": generate_day_count(),
+            "day_count_floating": generate_day_count(),
+        }
 
-    return IRSwapTrade(**swap_data)
+        return IRSwapTrade(**swap_data)
 
+    def _create_fx_option_trade(self) -> FXOptionTrade:
+        trade_date = generate_date()
+        expiry_date = generate_date(start_date=trade_date)
 
-def create_fx_option_trade() -> FXOptionTrade:
-    trade_date = generate_date()
-    expiry_date = generate_date(start_date=trade_date)
+        underlying = generate_ccy_pair()
+        notional_ccy = underlying.split("/")[-1]
+        fx_option_data = {
+            "trade_id": generate_trade_id(),
+            "trade_date": trade_date,
+            "asset_class": AssetClass.FX,
+            "product_type": ProductType.FX_OPTION,
+            "notional": generate_notional(),
+            "direction": generate_direction(),
+            "option_type": generate_option_type(),
+            "strike": generate_strike(),
+            "expiry_date": expiry_date,
+            "underlying": underlying,
+            "notional_ccy": Ccy[notional_ccy],
+            "pay_receive_fixed": generate_pay_receive(),
+        }
 
-    underlying = generate_ccy_pair()
-    notional_ccy = underlying.split("/")[-1]
-    fx_option_data = {
-        "trade_id": generate_trade_id(),
-        "trade_date": trade_date,
-        "asset_class": AssetClass.FX,
-        "product_type": ProductType.FX_OPTION,
-        "notional": generate_notional(),
-        "direction": generate_direction(),
-        "option_type": generate_option_type(),
-        "strike": generate_strike(),
-        "expiry_date": expiry_date,
-        "underlying": underlying,
-        "notional_ccy": Ccy[notional_ccy],
-        "pay_receive_fixed": generate_pay_receive(),
-    }
+        return FXOptionTrade(**fx_option_data)
 
-    return FXOptionTrade(**fx_option_data)
+    def _create_fx_spot_trade(self) -> FXTrade:
 
+        fx_spot_data = {
+            "trade_id": generate_trade_id(),
+            "trade_date": generate_date(),
+            "asset_class": AssetClass.FX,
+            "product_type": ProductType.FX_SPOT,
+            "notional": generate_notional(),
+            "ccy_pair": generate_ccy_pair(),
+            "direction": generate_direction(),
+            "value_date": generate_date(),
+        }
 
-def create_fx_spot_trade() -> FXTrade:
-
-    fx_spot_data = {
-        "trade_id": generate_trade_id(),
-        "trade_date": generate_date(),
-        "asset_class": AssetClass.FX,
-        "product_type": ProductType.FX_SPOT,
-        "notional": generate_notional(),
-        "ccy_pair": generate_ccy_pair(),
-        "direction": generate_direction(),
-        "value_date": generate_date(),
-    }
-
-    return FXTrade(**fx_spot_data)
+        return FXTrade(**fx_spot_data)
--- src/trade_service.py
+++ src/trade_service.py
@@ -1,24 +1,13 @@
 from src.common.enums import ProductType
 from src.models.trade_models import TradeCollection
-from src.factories.trade_factory import (
-    create_bullet_bond_trade,
-    create_ir_swap_trade,
-    create_fx_option_trade,
-    create_fx_spot_trade,
-)
+from src.factories.trade_factory import TradeFactory
 
 import random
 
 
 class TradeService:
-    def __init__(self):
-        self.FACTORY_MAP = {
-            ProductType.BOND_FIXED: create_bullet_bond_trade,
-            ProductType.IR_SWAP: create_ir_swap_trade,
-            ProductType.FX_OPTION: create_fx_option_trade,
-            ProductType.FX_SPOT: create_fx_spot_trade,
-        }
-        self.ALL_PRODUCT_TYPES = list(self.FACTORY_MAP.keys())
+    def __init__(self, factory: TradeFactory):
+        self.factory = factory
 
     # For large datasets we could have duplicate unique identifiers as these are randomly generated
     def create_trades(
@@ -28,15 +17,22 @@ class TradeService:
         sort: bool = True,
     ) -> TradeCollection:
         trades = TradeCollection()
-        available_types = product_types or self.ALL_PRODUCT_TYPES
+        available_types = product_types or list(ProductType)
         # Potentially need error handling
 
         for _ in range(n):
             product_type = random.choice(available_types)
-            trade = self.FACTORY_MAP[product_type]()
+            trade = self.factory.create(product_type)
             trades.append(trade)
 
         if sort:
             trades.sort(key=lambda t: t.trade_date, reverse=True)
 
         return trades
+
+
+if __name__ == "__main__":
+    service = TradeService(TradeFactory())
+    product_types = [ProductType.FX_OPTION]
+    trades = service.create_trades(product_types=product_types)
+    trades.to_csv("/home/josh/coding/trades_data_dummy/src/output.csv")
